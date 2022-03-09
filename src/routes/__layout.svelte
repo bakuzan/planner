d566ca2442cc686b5db1bdf0eb29b617772c11aa
bakuzan <imannoyed4days@hotmail.co.uk>
2022-03-09 08:55:38 +0000

    Sveltekit starting point with minimal changes

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,43 @@
+<nav class="app-bar">
+    <a class="app-bar__home" href="/">Planner</a>
+</nav>
+
+<div class="app-page">
+    <slot></slot>
+</div>
+
+<style lang="scss">
+    :root {
+        --top-bar-height: 50px;
+        --theme-colour: #66023c;
+        --constrast-colour: #fff;
+
+        font-size: 16px;
+        font-family: 'Lucida Console', Consolas, monospace;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    .app-bar {
+        background-color: var(--theme-colour);
+        color: var(--constrast-colour);
+        height: var(--top-bar-height);
+        width: 100%;
+        padding: 5px;
+        display:flex;
+        
+
+        &__home {
+            font-size: 1.5rem;
+            color: var(--constrast-colour);
+            text-decoration: none;
+            margin: auto 0;
+        }
+    }
+
+    .app-page {
+        padding: 0 5px;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+  <title>Home | Planner</title>
+</svelte:head>
+
+<h1 class="page-title">Home</h1>
+
+<style>
+
+  .page-title {
+    font-size: 2rem;
+    margin: 5px 0;
+  }
+</style>
